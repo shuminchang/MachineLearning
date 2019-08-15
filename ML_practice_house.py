7ecbb16091e484c173dcc7701f1f835b3190d9c0
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2019-08-15 20:29:28 +0800

    update
    add XGBRegressor

--- ML_practice_house.py
+++ ML_practice_house.py
@@ -129,3 +129,40 @@ model = RandomForestRegressor(n_estimators=500, random_state=0)
 scores = -1 * cross_val_score(model, OH_X_train, y, cv=5, scoring='neg_mean_absolute_error')
 
 print("MAE scores:\n", scores.mean())
+
+# XGBRegressor without missing values with Cross Validation
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X = pd.read_csv('../input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice              
+X.drop(['SalePrice'], axis=1, inplace=True)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X.columns if X[cname].nunique() < 10 and 
+                        X[cname].dtype == "object"]
+
+# Select numeric columns
+numeric_cols = [cname for cname in X.columns if X[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numeric_cols
+X_train = X[my_cols].copy()
+
+# One-hot encode the data (to shorten the code, we use pandas)
+X_train = pd.get_dummies(X_train)
+
+from xgboost import XGBRegressor
+from sklearn.model_selection import cross_val_score
+
+model = XGBRegressor(n_estimators=500, learning_rate=0.1)
+scores = -1 * cross_val_score(model, X_train, y, cv=5, scoring='neg_mean_absolute_error')
+
+print("MAE scores:\n", scores)
+print("MAE scores_mean:\n", scores.mean())
